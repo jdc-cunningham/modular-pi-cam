7f40f5a522057b124fdd86448ac2d12bf1564431
Jacob Cunningham <jcun4128@gmail.com>
2024-03-05 12:56:20 -0600

    same dimension as display

--- camera/pi-zero/software/camera/camera.py
+++ camera/pi-zero/software/camera/camera.py
@@ -15,7 +15,7 @@ class Camera:
     self.picam2 = Picamera2()
     self.encoder = H264Encoder()
     # dimensions based on 4608x2592 resolution, set for camera module v3
-    self.small_res_config = self.picam2.create_still_configuration(main={"size": (427, 240)}) # should not be a square
+    self.small_res_config = self.picam2.create_still_configuration(main={"size": (240, 240)}) # should not be a square
     self.full_res_config = self.picam2.create_still_configuration()
     self.last_mode = "small"
     self.live_preview_active = False
